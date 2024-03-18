316c893dbe3aebd13bfd01d2983e331da3d84788
Lukasz Kukla <59692808+lukaszkukla@users.noreply.github.com>
2024-03-18 14:34:18 +0000

    add too high/low functionality and messages

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-"use strict";
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number! 😊';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 9;
+// console.log(document.querySelector('.guess').value);
+
+let score = 20;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Enter a number 💩';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      '🎉🎉🎉 Correct number! 🎉🎉🎉';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
